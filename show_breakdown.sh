476e8f81b11f403be42e76ccda1ab6c17ecc0370
monotone-RK <kobaya4121@gmail.com>
2022-11-04 15:36:58 +0900

    チェックサムによる検証機能を追加

--- show_breakdown.sh
+++ show_breakdown.sh
@@ -17,18 +17,23 @@ CURRENT_LIST="$(cat "$1")"
 
 TOTAL=$(echo "$CURRENT_LIST" | wc -l)
 BREAKDOWN=$(
-while [ -n "$(echo "$CURRENT_LIST")" ]
-do
-    key=$(echo "$CURRENT_LIST" | head -n 1)
-    val=$(echo "$CURRENT_LIST" | grep "^$key$" | wc -l)
-    printf "%3s\t" $val
-    echo $key
-    CURRENT_LIST=$(echo "$CURRENT_LIST" | sed -e "/^$key$/d")
-done |
-    sort -rn -k1
+    while [ -n "$(echo "$CURRENT_LIST")" ]; do
+        key=$(echo "$CURRENT_LIST" | head -n 1)
+        val=$(echo "$CURRENT_LIST" | grep "^$key$" | wc -l)
+        printf "%3s\t" $val
+        echo $key
+        CURRENT_LIST=$(echo "$CURRENT_LIST" | sed -e "/^$key$/d")
+    done |
+        sort -rn -k1
 )
 
 # Show Result
+CHKSUM=$(echo "$BREAKDOWN" | awk '{a+=$1} END{print a;}')
+if [ $TOTAL != $CHKSUM ]; then
+    echo "Error. The totals for all items in the breakdown do not match the original data."
+    printf "The original data: %3s\n" $TOTAL
+    printf "The totals for all items in the breakdown: %3s\n\n" $CHKSUM
+fi
 printf "%3s\t" $TOTAL
 echo "Total ("$(echo "$BREAKDOWN" | wc -l) "categories)"
 echo "=============================="
